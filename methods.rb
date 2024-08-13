e7073b08dd27fde39d3136f1c92fd2f84a51e4d8
silvante <khamidov.ko@gmail.com>
2024-08-13 13:08:48 +0500

    fix

--- methods.rb
+++ methods.rb
@@ -1,12 +1,17 @@
-# @ex1 = 1
+@ex1 = 5
 
 # def function
 #   puts "def body..."
 # end
 
-# def FWP(num)
-#   puts @ex1 + num
-# end
+def FWP(num)
+  if num > @ex1
+    puts "katta"
+  elsif num < @ex1
+    puts "kichkina"
+  else
+    puts "teng bosa kereda"
+  end
+end
 
-# FWP( 3 )
-
+FWP( 5 )
