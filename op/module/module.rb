23463525f669006f3359d19f851df060a6a02738
silvante <khamidov.ko@gmail.com>
2024-08-14 16:48:46 +0500

    fix

---
+++ op/module/module.rb
@@ -0,0 +1,5 @@
+require_relative "tools.rb"
+include Tools
+
+Tools.seyHi("mardonbek")
+Tools.seyBye("mardonbek")
---
+++ op/module/tools.rb
@@ -0,0 +1,8 @@
+module Tools
+  def seyHi(name)
+    puts "Ciao, #{name}"
+  end
+  def seyBye(name)
+    puts "Arrividercie, #{name}"
+  end
+end
