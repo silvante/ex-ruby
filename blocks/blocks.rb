4a0b221ff544da6de8e2c4e06922f6e7c7301ebe
silvante <khamidov.ko@gmail.com>
2024-08-15 13:08:19 +0500

    lesson 18

--- blocks/blocks.rb
+++ blocks/blocks.rb
@@ -1,8 +1,12 @@
 array = %w[jonatan joseph jotaro josuke giorno]
 
+# creating block
 array.each do |jojo|
   if jojo == "giorno"
     array.push "jolyne"
   end
   puts jojo
 end
+
+# creating block easy way
+array.each { |i| puts i  }
